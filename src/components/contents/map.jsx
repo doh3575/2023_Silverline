b4f95f6cb70002d59bc8cceaf256d092f2f3b9f3
kimdohee <doh3575@gmail.com>
2023-05-07 18:05:16 +0900

    sangdo1

--- src/components/contents/map.jsx
+++ src/components/contents/map.jsx
@@ -8,20 +8,12 @@ const MapComponent = ({ value }) => {
   const [convertData, setConvertData] = useState([]);
   const positions = [
     {
-      title: "카카오",
-      latlng: { lat: 33.450705, lng: 126.570677 },
+      title: "상도1동주민센터",
+      latlng: { lat: 37.4980901, lng: 126.953061 },
     },
     {
-      title: "생태연못",
-      latlng: { lat: 33.450936, lng: 126.569477 },
-    },
-    {
-      title: "텃밭",
-      latlng: { lat: 33.450879, lng: 126.56994 },
-    },
-    {
-      title: "근린공원",
-      latlng: { lat: 33.451393, lng: 126.570738 },
+      title: "상도패리스경로당",
+      latlng: { lat: 37.5035055, lng: 126.953951 },
     },
   ];
 

2a7082bc848e88c3965fdc60c01439408dc94043
kimdohee <doh3575@gmail.com>
2023-05-07 18:01:31 +0900

    jeju3

--- src/components/contents/map.jsx
+++ src/components/contents/map.jsx
@@ -4,27 +4,26 @@ import { Map, Polyline } from "react-kakao-maps-sdk";
 import markerData from "@/assets/markerData";
 import { getResult } from "@/assets/utils";
 
-const positions = [
-  {
-    title: "카카오",
-    latlng: { lat: 33.450705, lng: 126.570677 },
-  },
-  {
-    title: "생태연못",
-    latlng: { lat: 33.450936, lng: 126.569477 },
-  },
-  {
-    title: "텃밭",
-    latlng: { lat: 33.450879, lng: 126.56994 },
-  },
-  {
-    title: "근린공원",
-    latlng: { lat: 33.451393, lng: 126.570738 },
-  },
-];
-
 const MapComponent = ({ value }) => {
   const [convertData, setConvertData] = useState([]);
+  const positions = [
+    {
+      title: "카카오",
+      latlng: { lat: 33.450705, lng: 126.570677 },
+    },
+    {
+      title: "생태연못",
+      latlng: { lat: 33.450936, lng: 126.569477 },
+    },
+    {
+      title: "텃밭",
+      latlng: { lat: 33.450879, lng: 126.56994 },
+    },
+    {
+      title: "근린공원",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+    },
+  ];
 
   useEffect(() => {
     if (value) {

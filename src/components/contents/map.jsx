7365c161f21eb471e8ab60a954865f31e3f4f519
kimdohee <doh3575@gmail.com>
2023-05-07 17:51:45 +0900

    jeju

--- src/components/contents/map.jsx
+++ src/components/contents/map.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Map, Marker, Polyline } from "react-kakao-maps-sdk";
+import { Map, Polyline } from "react-kakao-maps-sdk";
 import markerData from "@/assets/markerData";
 import { getResult } from "@/assets/utils";
 
@@ -13,6 +13,25 @@ const MapComponent = ({ value }) => {
     }
   }, [value]);
 
+  const positions = [
+    {
+      title: "카카오",
+      latlng: { lat: 33.450705, lng: 126.570677 },
+    },
+    {
+      title: "생태연못",
+      latlng: { lat: 33.450936, lng: 126.569477 },
+    },
+    {
+      title: "텃밭",
+      latlng: { lat: 33.450879, lng: 126.56994 },
+    },
+    {
+      title: "근린공원",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+    },
+  ];
+
   return (
     <section className="map-section">
       <Map
@@ -20,6 +39,21 @@ const MapComponent = ({ value }) => {
         style={{ width: "100%", height: "100%" }}
         level={4}
       >
+        {positions.map((position, index) => (
+          <MapMarker
+            key={`${position.title}-${position.latlng}`}
+            position={position.latlng} // 마커를 표시할 위치
+            image={{
+              src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
+              size: {
+                width: 24,
+                height: 35,
+              }, // 마커이미지의 크기입니다
+            }}
+            title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          />
+        ))}
+
         {!!convertData.length && (
           <>
             {convertData.map((data) => (

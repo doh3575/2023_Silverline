35eaf1cdfde9a2374216d1e486138cefdab9e06d
kimdohee <doh3575@gmail.com>
2023-05-08 16:41:56 +0900

    roadview 3

--- src/components/contents/map.jsx
+++ src/components/contents/map.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Map, MapMarker, Polyline } from "react-kakao-maps-sdk";
 import { getResult } from "@/assets/utils";
 
@@ -67,6 +67,15 @@ const MapComponent = ({ value }) => {
       latlng: { lat: 37.4953952, lng: 126.950858 },
     },
   ];
+  const [isAtive, setIsAtive] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const mapRef = useRef();
+  const roadviewRef = useRef();
+
+  const [center, setCenter] = useState({
+    lat: 37.503223613853585,
+    lng: 126.95167472871846,
+  });
 
   useEffect(() => {
     if (value) {
@@ -74,12 +83,23 @@ const MapComponent = ({ value }) => {
     }
   }, [value]);
 
+  useEffect(() => {
+    const map = mapRef.current;
+    const roadview = roadviewRef.current;
+    if (roadview && map) {
+      roadview.relayout();
+      map.relayout();
+      map.setCenter(new kakao.maps.LatLng(center.lat, center.lng));
+    }
+  }, [isVisible, center, isAtive]);
+
   return (
     <section className="map-section">
       <Map
         center={{ lat: 37.503223613853585, lng: 126.95167472871846 }}
         style={{ width: "100%", height: "100%" }}
         level={4}
+        ref={mapRef}
       >
         {positions.map((position, index) => (
           <MapMarker
@@ -110,6 +130,101 @@ const MapComponent = ({ value }) => {
         )}
       </Map>
     </section>
+  );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <RoadviewWithMapButtonStyle />
+      <Map // 로드뷰를 표시할 Container
+        center={center}
+        style={{
+          // 지도의 크기
+          width: !isVisible ? "100%" : "50%",
+          height: "100%",
+        }}
+        level={3}
+        ref={mapRef}
+      >
+        {isAtive && (
+          <>
+            <MapTypeId type={kakao.maps.MapTypeId.ROADVIEW} />
+            <MapMarker
+              position={center}
+              draggable={true}
+              onDragEnd={(marker) => {
+                setCenter({
+                  lat: marker.getPosition().getLat(),
+                  lng: marker.getPosition().getLng(),
+                });
+              }}
+              image={{
+                src: "https://t1.daumcdn.net/localimg/localimages/07/2018/pc/roadview_minimap_wk_2018.png",
+                size: { width: 26, height: 46 },
+                options: {
+                  spriteSize: { width: 1666, height: 168 },
+                  spriteOrigin: { x: 705, y: 114 },
+                  offset: { x: 13, y: 46 },
+                },
+              }}
+            />
+          </>
+        )}
+      </Map>
+      <div
+        id="roadviewControl"
+        className={isAtive ? "active" : ""}
+        onClick={() => {
+          setIsVisible(true);
+          setIsAtive(!isAtive);
+        }}
+      >
+        <span className="img"></span>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          width: isVisible ? "50%" : "0",
+          overflow: "hidden",
+        }}
+      >
+        <Roadview // 로드뷰를 표시할 Container
+          position={{ ...center, radius: 50 }}
+          style={{
+            // 지도의 크기
+            width: "100%",
+            height: "100%",
+          }}
+          onPositionChanged={(rv) => {
+            setCenter({
+              lat: rv.getPosition().getLat(),
+              lng: rv.getPosition().getLng(),
+            });
+          }}
+          onPanoidChange={() => {
+            isAtive && setIsVisible(true);
+          }}
+          onErrorGetNearestPanoId={() => {
+            setIsVisible(false);
+          }}
+          ref={roadviewRef}
+        >
+          <div
+            id="close"
+            title="로드뷰닫기"
+            onClick={() => setIsVisible(false)}
+          >
+            <span className="img"></span>
+          </div>
+        </Roadview>
+      </div>
+    </div>
   );
 };
 

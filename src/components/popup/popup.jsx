7ad3462aa9cf344c30c309fd2812827e0d791c17
kimdohee <doh3575@gmail.com>
2023-05-15 01:56:34 +0900

    popup14

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -16,12 +16,16 @@ const Popup = () => {
   return (
     <>
       <button className="popup-button" onClick={togglePopup}>
-        {open ? "X" : "?"}
+        {open ? "x" : "?"}
       </button>
       <div className={popupClassName}>
         <h1>팝업창</h1>
         <p>팝업창 내용을 여기에 작성하세요.</p>
-        <img src="https://ifh.cc/g/W0sr2l.png" alt="이미지" />
+        <img
+          src="https://ifh.cc/g/W0sr2l.png"
+          alt="이미지"
+          style={{ width: "200px", height: "150px" }}
+        />
       </div>
     </>
   );

fee6d71a9b6d250691ff059d7d18acb113fa7180
kimdohee <doh3575@gmail.com>
2023-05-15 02:28:48 +0900

    popup1

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -19,12 +19,10 @@ const Popup = () => {
         {open ? "x" : "?"}
       </button>
       <div className={popupClassName}>
-        <h1>팝업창</h1>
-        <p>팝업창 내용을 여기에 작성하세요.</p>
         <img
-          src="https://ifh.cc/g/W0sr2l.png"
+          src="https://ifh.cc/v-hTvdRf.png"
           alt="이미지"
-          style={{ width: "200px", height: "150px" }}
+          style={{ width: "40%", height: "60%" }}
         />
       </div>
     </>

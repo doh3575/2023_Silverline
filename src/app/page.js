200991a1040536c4e13c30dcf22eb5a99f718f68
kimdohee <doh3575@gmail.com>
2023-05-15 01:16:30 +0900

    popup8

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
     <main>
       <h3 style={{ marginBottom: "10px" }}>노인안전보행길</h3>
       <h1>Silver Line</h1>
+      <Popup />
       <Gradient />
       <Contents />
-      <Popup />
       <footer className="footer">
         서울시립대학교 2023 공간정보종합설계 22조 김도희 장민
       </footer>

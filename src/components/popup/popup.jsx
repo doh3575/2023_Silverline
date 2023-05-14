615319b7b4338ec0b67c4222becabcb7f6236fbf
kimdohee <doh3575@gmail.com>
2023-05-14 21:13:33 +0900

    popup5

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -11,6 +11,7 @@ const Popup = ({ open, onClose }) => {
       <button className="popup-button" onClick={onClose}>
         {open ? "?" : "X"}
       </button>
+
       <div className={popupClassName}>
         <h1>팝업창</h1>
         <p>팝업창 내용을 여기에 작성하세요.</p>

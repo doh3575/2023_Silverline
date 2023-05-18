d9fb5edf8811475496a34b24e1eb672062f3848b
kimdohee <doh3575@gmail.com>
2023-05-18 20:24:49 +0900

    default2

--- src/components/contents/contents.jsx
+++ src/components/contents/contents.jsx
@@ -1,37 +1,27 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import Description from './description';
-import Map from './map';
-import Select from './select-wrap';
-import './styles.css';
+"use client";
+import React, { useEffect, useState } from "react";
+import Description from "./description";
+import Map from "./map";
+import Select from "./select-wrap";
+import "./styles.css";
 
 export default function Contents() {
   const [value, setValue] = useState({
-    first: '',
-    second: '',
-    third: '',
-    fourth: '',
+    first: "",
+    second: "",
+    third: "",
+    fourth: "",
   });
-
-  const [fin, setFin] = useState(false);
 
   const handleSelect = (rank, value) => {
     setValue((prev) => ({ ...prev, [rank]: value }));
   };
 
-  useEffect(() => {
-    if (value.first && value.second && value.third && value.fourth) {
-      setFin(true);
-      return;
-    }
-    setFin(false);
-  }, [value]);
-
   return (
-    <article className='contents'>
+    <article className="contents">
       <Select onSelect={handleSelect} />
       <Description />
-      <Map value={fin ? value : undefined} />
+      <Map value={value} />
     </article>
   );
 }
